575ed262632022e9407717d64330bcdfc34b0d55
Patrick Blood <pblood66@gmail.com>
2026-06-26 11:29:15 -0600

    Centered the map

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -2,8 +2,16 @@ import { SkyMap, type SkyMapProps } from "../components/SkyMap";
 
 export const MapPage = (props: SkyMapProps) => {
     return (
-        <>
+        <div
+        style={
+            {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh'
+            }
+        }>
             <SkyMap passes={props.passes} showNames={props.showNames} />
-        </>
+        </div>
     )
 }
