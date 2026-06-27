887c1e4fec703e9bc469c7aac1fbbea3ec8bfb1a
Patrick Blood <pblood66@gmail.com>
2026-06-26 17:12:33 -0700

    update 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
   base: '/SatOnTheHat/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-index-to-404',
+      closeBundle() {
+        copyFileSync('dist/index.html', 'dist/404.html')
+      },
+    },
+  ],
   optimizeDeps: {
     exclude: ['satellite.js']
   },
