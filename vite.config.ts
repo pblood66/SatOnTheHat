5db3bb0f1b707d9764c9a41cd04d152113b509d4
Patrick Blood <pblood66@gmail.com>
2026-06-29 14:53:11 -0600

    update title and base path for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { copyFileSync } from 'fs'
 
 export default defineConfig({
-  base: '/SatOnTheHat/',
+  base: '/overpass/',
   plugins: [
     react(),
     {
